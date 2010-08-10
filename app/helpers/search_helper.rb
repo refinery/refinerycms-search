34db75914ed37795a06d70628fb52586815969b3
Amanda Wagener <amanda@resolvedigital.co.nz>
2010-08-10 16:16:23 +1200

    Use url_for to generate search result urls

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -3,15 +3,7 @@ module SearchHelper
 	# this is where you register your result URLs based on the
 	# type of result we're dealing with
 	def result_url(result)
-		table_name = result.class.to_s.tableize
-		case table_name
-		when "news_items"
-			"/news/#{result.to_param}"
-		when "pages"
-		  result.url
-		else
-			"/#{table_name}/#{result.to_param}"
-		end
+		result.respond_to?(:url) ? result.url : url_for(result)
 	end
 	
 end
