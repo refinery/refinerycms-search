da3f2c68dcf122f834cadee2ff7be215a1e8ed5a
Uģis Ozols <ugis.ozolss@gmail.com>
2011-09-07 08:18:32 +0300

    Don't show search results page in menu.

--- db/seeds/refinerycms_search.rb
+++ db/seeds/refinerycms_search.rb
@@ -11,6 +11,7 @@ if defined?(::Page)
   unless Page.where(:menu_match => "^/search.*$").any?
     page = Page.create(
       :title => "Search Results",
+      :show_in_menu => false,
       :link_url => "/search",
       :deletable => false,
       :position => ((Page.maximum(:position, :conditions => {:parent_id => nil}) || -1)+1),
