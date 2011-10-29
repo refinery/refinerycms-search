354d5664247e3ac11615495f7bf2ac60999c97a1
Philip Arndt <parndt@gmail.com>
2011-10-29 14:01:54 +1300

    Tabs to spaces?!

--- app/models/search_engine.rb
+++ app/models/search_engine.rb
@@ -1,17 +1,17 @@
 class SearchEngine
 
-	# How many results should we show per page
-	RESULTS_LIMIT = 10
+  # How many results should we show per page
+  RESULTS_LIMIT = 10
 
-	# Perform search over the specified models
-	def self.search(query, page = 1)
-		results = []
+  # Perform search over the specified models
+  def self.search(query, page = 1)
+    results = []
 
-		Refinery.searchable_models.each do |model|
-			results << model.limit(RESULTS_LIMIT).with_query(query)
-		end if query.present?
+    Refinery.searchable_models.each do |model|
+      results << model.limit(RESULTS_LIMIT).with_query(query)
+    end if query.present?
 
-		results.flatten[0..(RESULTS_LIMIT - 1)]
-	end
+    results.flatten[0..(RESULTS_LIMIT - 1)]
+  end
 
 end
