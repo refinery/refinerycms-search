8597091a2f4be8139c9b90e994f202a81c05b1c8
Anita Graham <anita@joli.com.au>
2015-02-03 20:19:29 +0800

    Compatible with Refinerycms 3.0.1

--- spec/models/refinery/search_engine_spec.rb
+++ spec/models/refinery/search_engine_spec.rb
@@ -6,18 +6,18 @@ module Refinery
       context "when page exist" do
         # we're using page factory because search engine uses
         # page model as default model
-        let!(:page) { FactoryGirl.create(:page, :title => "testy") }
+        let!(:page) { FactoryGirl.create(:page, title: "testy") }
 
         it "returns an array consisting of mathcing pages" do
           result = SearchEngine.search("testy")
-          result.should include(page)
+          expect(result).to include(page)
         end
       end
 
       context "when page does not exist" do
         it "returns empty array" do
           result = SearchEngine.search("ugisozols")
-          result.should be_empty
+          expect(result).to be_empty
         end
       end
     end
